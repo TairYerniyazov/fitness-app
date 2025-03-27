631fde7b1ef99e1beb216e13ad1e2f7d3739da49
Janszczyrek <janwalus@gmail.com>
2025-03-27 10:24:41 +0100

    add colors for exercisekind button

--- app/src/main/java/uj/lab/fitnessapp/ui/theme/Color.kt
+++ app/src/main/java/uj/lab/fitnessapp/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val favoriteColor = Color(0xFFF6AE2D)
+val cardioColor = Color(0xFF17C9E8)
+val strengthColor = Color(0xFFE83617)
