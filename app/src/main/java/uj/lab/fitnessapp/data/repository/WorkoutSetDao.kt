8bb3cef2f44b0ef4cfe45ac5dc75d0b49a4c31e5
sosna0 <ksosnowski01@gmail.com>
2025-04-01 16:51:17 +0200

    Further changes in database

--- app/src/main/java/uj/lab/fitnessapp/data/repository/WorkoutSetDao.kt
+++ app/src/main/java/uj/lab/fitnessapp/data/repository/WorkoutSetDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Update
-import uj.lab.fitnessapp.data.model.Exercise
 import uj.lab.fitnessapp.data.model.WorkoutSet
 
 @Dao
