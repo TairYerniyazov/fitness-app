370935c95dbe1eff6ac38ac5168bca3124188e1d
gregwojcik <grzegorz02.wojcik@student.uj.edu.pl>
2025-06-02 12:14:02 +0200

    Changed size of a floation button to make text fit in one line

--- app/src/main/java/uj/lab/fitnessapp/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/uj/lab/fitnessapp/ui/screen/home/HomeScreen.kt
@@ -156,7 +156,7 @@ fun HomeScreen(navController: NavController) {
                 modifier = Modifier
                     .padding(18.dp)
                     .height(56.dp)
-                    .fillMaxWidth(0.6f)
+                    .fillMaxWidth(0.7f)
             ) {
                 Text(
                     "Dodaj instancję ćwiczenia",
